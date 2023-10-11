db8372234f120686d562fa6095eff6df8cc9b899
Freedy-FR <freedy.ramos@hotmail.com>
2023-10-11 19:41:10 +0000

    Added sample text

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+main_text = "This is a test to center the main console"
+
+print(main_text)
