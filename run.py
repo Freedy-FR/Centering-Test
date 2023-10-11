48c76964d2003af4e84c6a137d1ebc034343518e
Freedy-FR <freedy.ramos@hotmail.com>
2023-10-11 19:45:56 +0000

    change variable to uppercase

--- run.py
+++ run.py
@@ -2,6 +2,6 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-main_text = "This is a test to center the main console"
+MAINTEXT = "This is a test to center the main console"
 
-print(main_text)
+print(MAINTEXT)
